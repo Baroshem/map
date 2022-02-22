726ab298442f079335be98849a51685c1907f9de
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-02-22 10:18:25 +0100

    feat: create NuxtMap and NuxtMarker

--- src/runtime/components/NuxtMap.vue
+++ src/runtime/components/NuxtMap.vue
@@ -1,25 +1,56 @@
 <template>
-  <google-map
-    style="width: 100%; height: 500px"
-    :center="center"
-    :zoom="15"
+  <g-map
+    v-if="provider == 'google'"
+    :center="options.center"
+    :zoom="options.zoom"
     >
     <slot name="marker" />
-  </google-map>
+  </g-map>
+  <div v-else-if="provider === 'leaflet'" id="mapid">
+    <slot name="marker"/>
+  </div>
 </template>
 
 <script lang="ts">
-import { GoogleMap } from 'vue3-google-map'
 
-export default{
+import { onMounted, defineComponent, defineAsyncComponent } from "vue";
+
+export default defineComponent({
   name: 'NuxtMap',
-  components: { GoogleMap },
+  components: {
+    GMap: defineAsyncComponent(() => import('./providers/google/GMap.vue')),
+    // LMap: defineAsyncComponent(() => import('./providers/leaflet/LMap.vue'))
+  },
   props: {
     // TODO: Add better types here
     options: {
       type: Object,
       default: () => ({})
+    },
+    provider: {
+      type: String,
+      required: true,
     }
   },
-}
+  setup({ provider }) {
+    if (provider === 'leaflet') {
+      let mymap;
+      let leaflet;
+      onMounted(async () => {
+        await import("leaflet/dist/leaflet.css");
+
+        leaflet = await import('leaflet')
+        mymap = leaflet.map("mapid").setView([42.5145, -83.0147], 9);
+
+        leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(mymap);
+
+        leaflet.marker([51.5, -0.09]).addTo(mymap)
+          .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+          .openPopup();
+      })
+    }
+  }
+})
 </script>
--- src/runtime/components/NuxtMarker.vue
+++ src/runtime/components/NuxtMarker.vue
@@ -1,19 +1,27 @@
 <template>
-  <Marker :options="options" />
+  <google-marker v-if="provider === 'google'" :options="options"/>
+  <!-- <leaflet-marker v-else-if="provider === 'leaflet'" :options="options"/> -->
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import { Marker } from 'vue3-google-map'
+import { defineComponent, defineAsyncComponent } from 'vue'
 
 export default defineComponent({
   name: 'NuxtMarker',
-  components: { Marker },
+  components: {
+    // Marker
+    GMarker: defineAsyncComponent(() => import('./providers/google/GMarker.vue')),
+    // LMarker: defineAsyncComponent(() => import('./providers/leaflet/LMarker.vue'))
+  },
   props: {
     // TODO: add types for options https://developers.google.com/maps/documentation/javascript/reference/marker#MarkerOptions
     options: {
       type: Object,
       default: () => ({})
+    },
+    provider: {
+      type: String,
+      required: true,
     }
   },
 })
