6f89fde9e4cee24e7c8d072e3b531362e1b00a65
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-02-22 10:17:57 +0100

    feat: create LMap and LMarker

---
+++ src/runtime/components/providers/leaflet/LMap.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    :center="options.center"
+    :zoom="options.zoom"
+  >
+    Leaflet Map
+    <slot name="marker" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, defineAsyncComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LMap',
+  props: {
+    // TODO: add types for options https://developers.google.com/maps/documentation/javascript/reference/marker#MarkerOptions
+    options: {
+      type: Object,
+      default: () => ({})
+    },
+  },
+})
+</script>
+
---
+++ src/runtime/components/providers/leaflet/LMarker.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Leaflet Marker</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LMarker',
+  props: {
+    // TODO: add types for options https://developers.google.com/maps/documentation/javascript/reference/marker#MarkerOptions
+    options: {
+      type: Object,
+      default: () => ({})
+    },
+  },
+})
+</script>
