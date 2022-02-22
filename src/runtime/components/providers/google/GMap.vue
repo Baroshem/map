4b986ead3d9dbe3ecf1fe07c52f178dcfacfc1f5
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-02-22 10:18:10 +0100

    feat: create GMap and GMarker

---
+++ src/runtime/components/providers/google/GMap.vue
@@ -0,0 +1,28 @@
+<template>
+  <google-map
+    :center="options.center"
+    :zoom="options.zoom"
+  >
+    <slot name="marker" />
+  </google-map>
+</template>
+
+<script lang="ts">
+import { GoogleMap } from 'vue3-google-map'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'GMap',
+  components: {
+    GoogleMap
+  },
+  props: {
+    // TODO: add types for options https://developers.google.com/maps/documentation/javascript/reference/marker#MarkerOptions
+    options: {
+      type: Object,
+      default: () => ({})
+    },
+  },
+})
+</script>
+
---
+++ src/runtime/components/providers/google/GMarker.vue
@@ -0,0 +1,22 @@
+<template>
+  <marker :options="options"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Marker } from 'vue3-google-map'
+
+export default defineComponent({
+  name: 'GMarker',
+  components: {
+    Marker
+  },
+  props: {
+    // TODO: add types for options https://developers.google.com/maps/documentation/javascript/reference/marker#MarkerOptions
+    options: {
+      type: Object,
+      default: () => ({})
+    },
+  },
+})
+</script>
