21adb85ba68dcc6dd15f68ef51acba899929d0b7
Conner Bachmann <59223342+Intevel@users.noreply.github.com>
2022-03-27 09:53:33 +0200

    fix: cannot use .ts' extension.

--- src/runtime/components/NuxtMarker.vue
+++ src/runtime/components/NuxtMarker.vue
@@ -1,46 +1,56 @@
 <template>
-  <component :is="providerComponentName" :options="options" :popup="popup"></component>
+  <component
+    :is="providerComponentName"
+    :options="options"
+    :popup="popup"
+  ></component>
 </template>
 
 <script lang="ts">
-import { useMapProviderConfig } from '../composables/useMapProviderConfig.ts'
-import { defineComponent, defineAsyncComponent, computed } from 'vue'
+import { useMapProviderConfig } from "../composables/useMapProviderConfig";
+import { defineComponent, defineAsyncComponent, computed } from "vue";
 
 export default defineComponent({
-  name: 'NuxtMarker',
+  name: "NuxtMarker",
   components: {
-    GMarker: defineAsyncComponent(() => import('./providers/google/GMarker.vue')),
-    LMarker: defineAsyncComponent(() => import('./providers/leaflet/LMarker.vue')),
-    MMarker: defineAsyncComponent(() => import('./providers/mapbox/MMarker.vue'))
+    GMarker: defineAsyncComponent(
+      () => import("./providers/google/GMarker.vue")
+    ),
+    LMarker: defineAsyncComponent(
+      () => import("./providers/leaflet/LMarker.vue")
+    ),
+    MMarker: defineAsyncComponent(
+      () => import("./providers/mapbox/MMarker.vue")
+    ),
   },
   props: {
     options: {
       type: Object,
       default: () => ({
         center: { lat: 40.689247, lng: -74.044502 },
-      })
+      }),
     },
     popup: {
       type: String,
-      default: ''
-    }
+      default: "",
+    },
   },
   setup() {
-    const { provider } = useMapProviderConfig()
+    const { provider } = useMapProviderConfig();
     const providerComponentName = computed(() => {
-      if (provider === 'google') {
-        return 'GMarker'
-      } else if (provider === 'leaflet') {
-        return 'LMarker'
-      } else if (provider === 'mapbox') {
-        return 'MMarker'
+      if (provider === "google") {
+        return "GMarker";
+      } else if (provider === "leaflet") {
+        return "LMarker";
+      } else if (provider === "mapbox") {
+        return "MMarker";
       }
       // TODO: add more providers here
-    })
+    });
 
     return {
-      providerComponentName
-    }
-  }
-})
+      providerComponentName,
+    };
+  },
+});
 </script>
