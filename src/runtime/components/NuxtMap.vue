e2a6cc85b53ba632a9f3c9462f5733043c89d637
Conner Bachmann <59223342+Intevel@users.noreply.github.com>
2022-03-27 09:26:31 +0200

    fix: import issues

--- src/runtime/components/NuxtMap.vue
+++ src/runtime/components/NuxtMap.vue
@@ -3,48 +3,49 @@
     :is="providerComponentName"
     :options="options"
     :apiKey="mapProviderConfig.options?.apiKey"
-  ><slot /></component>
+    ><slot
+  /></component>
 </template>
 
 <script lang="ts">
-
-import { useMapProviderConfig } from '../composables/useMapProviderConfig.ts'
-import { defineComponent } from "vue";
+import { useMapProviderConfig } from "../composables/useMapProviderConfig";
+import { defineComponent, defineAsyncComponent } from "vue";
 
 export default defineComponent({
-  name: 'NuxtMap',
+  name: "NuxtMap",
   components: {
-    GMap: defineAsyncComponent(() => import('./providers/google/GMap.vue')),
-    LMap: defineAsyncComponent(() => import('./providers/leaflet/LMap.vue')),
-    MMap: defineAsyncComponent(() => import('./providers/mapbox/MMap.vue')),
+    GMap: defineAsyncComponent(() => import("./providers/google/GMap.vue")),
+    LMap: defineAsyncComponent(() => import("./providers/leaflet/LMap.vue")),
+    MMap: defineAsyncComponent(() => import("./providers/mapbox/MMap.vue")),
   },
   props: {
     // TODO: Add better types here
+    theme: "aubergine",
     options: {
       type: Object,
       default: () => ({
         center: { lat: 40.689247, lng: -74.044502 },
-        zoom: 15
-      })
+        zoom: 15,
+      }),
     },
   },
   setup() {
-    const mapProviderConfig = useMapProviderConfig()
+    const mapProviderConfig = useMapProviderConfig();
     const providerComponentName = computed(() => {
-      if (mapProviderConfig.provider === 'google') {
-        return 'GMap'
-      } else if (mapProviderConfig.provider === 'leaflet') {
-        return 'LMap'
-      } else if (mapProviderConfig.provider === 'mapbox') {
-        return 'MMap'
+      if (mapProviderConfig.provider === "google") {
+        return "GMap";
+      } else if (mapProviderConfig.provider === "leaflet") {
+        return "LMap";
+      } else if (mapProviderConfig.provider === "mapbox") {
+        return "MMap";
       }
       // TODO: add more providers here
-    })
+    });
 
     return {
       providerComponentName,
-      mapProviderConfig
-    }
-  }
-})
+      mapProviderConfig,
+    };
+  },
+});
 </script>
